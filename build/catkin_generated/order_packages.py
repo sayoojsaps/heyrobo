98c80b52bba77e9c47e5a5f7136b37a073ed421c
sayoojsaps <sayoojputtankiyil@gmail.com>
2021-09-27 19:43:03 +0530

    begins

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/sayooj/heyrobo_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/sayooj/heyrobo_ws/devel;/home/sayooj/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/sayooj/heyrobo_ws/devel;/home/sayooj/catkin_ws/devel;/opt/ros/noetic' != '' else []
